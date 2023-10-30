ec7f7814ef3fde86257bc51dfbd9c66db6eaef44
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2023-10-29 23:49:48 -0300

    up

--- Erosion/Program.cs
+++ Erosion/Program.cs
@@ -1,53 +1,70 @@
 ﻿using SkiaSharp;
 using System;
+using System.Reflection;
 
 namespace Projeto {
 	class Program {
 
 		static void Main(string[] args) {
-			using (SKBitmap bitmapEntrada = SKBitmap.Decode("C:\\Users\\joaol\\Documents\\GitHub\\CognitiveComputing\\Erosion\\Floresta Foto.jpg"),
+			using (SKBitmap bitmapEntrada = SKBitmap.Decode("C:\\Users\\joaol\\Documents\\GitHub\\CognitiveComputing\\Erosion\\Floresta foto.jpg"),
 				bitmapSaida = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8))) {
 
-				Console.WriteLine(bitmapEntrada.ColorType);
-				Console.WriteLine(bitmapSaida.ColorType);
+				int tamanhoJanela = 3;
+				int metadeJanela = tamanhoJanela / 2;
 
 				unsafe {
 					byte* entrada = (byte*)bitmapEntrada.GetPixels();
 					byte* saida = (byte*)bitmapSaida.GetPixels();
 
-					int pixelsTotais = bitmapEntrada.Width * bitmapEntrada.Height;
-
-					// e controla a entrada, e vai de 4 em 4 (cada uma das componentes)
-					// s controla a saída, e vai de 1 em 1
-					for (int e = 0, s = 0; s < pixelsTotais; e += 4, s++) {
-						// Para uma imagem BGRA, entrada[e] é a componente B, entrada[e + 1] é G,
-						// entrada[e + 2] é R e entrada[e + 3] é A.
-						// Como a saída é em escala de cinza, só existe uma componente: saida[s]
-						// Aqui, vamos copiar a componente R da entrada para a saída:
-						saida[s] = (byte)((entrada[e] + entrada[e + 1] + entrada[e + 2]) / 3);
-					}
-
-					int largura = bitmapEntrada.Width;
-					int altura = bitmapEntrada.Height;
-					int tamanhoJanela = 3;
-
-					int metadeJanela = (int)Math.Ceiling((decimal)tamanhoJanela / 2);
-
-					for (int y = 0; y < altura; y++)
+					for (int y = 0; y < bitmapEntrada.Height; y++)
 					{
-						for (int x = 0; x < largura; x++)
+						for (int x = 0; x < bitmapEntrada.Width; x++)
 						{
-							
-							saida[y, x] = Math.Min(entrada[y - metadeJanela, x - metadeJanela], entrada[y + metadeJanela, x + metadeJanela]);
+							int i = (y * bitmapEntrada.Width) + x;
+							saida[i] = menorValorRegiao(entrada, bitmapEntrada.Width, bitmapSaida.Height, x, y, metadeJanela);
 						}
 					}
 
 				}
 
-				using (FileStream stream = new FileStream("C:\\Users\\joaol\\Documents\\GitHub\\CognitiveComputing\\Erosion\\Floresta Foto Teste.jpg", FileMode.OpenOrCreate, FileAccess.Write)) {
+				using (FileStream stream = new FileStream("C:\\Users\\joaol\\Documents\\GitHub\\CognitiveComputing\\Erosion\\floresta_teste.jpg", FileMode.OpenOrCreate, FileAccess.Write)) {
 					bitmapSaida.Encode(stream, SKEncodedImageFormat.Png, 100);
 				}
 			}
+		}
+
+		static unsafe byte menorValorRegiao(byte* entrada, int largura, int altura, int x, int y, int metadeJanela)
+		{
+			int xInicial = x - metadeJanela;
+			int yInicial = y - metadeJanela;
+			int xFinal = x + metadeJanela;
+			int yFinal = y + metadeJanela;
+
+			if (xInicial < 0)
+				xInicial = 0;
+
+			if (yInicial < 0)
+				yInicial = 0;
+
+			if (xFinal > largura)
+				xFinal = largura - 1;
+			
+			if (yFinal > altura)
+				yFinal = altura - 1;
+
+			byte menor = entrada[(yInicial * largura) + xInicial];		
+
+			for (y = yInicial; y <= yFinal; y++)
+			{
+				for (x = xInicial; x <= xFinal; x++)
+				{
+					int i = (y * largura) + x;
+					if (entrada[i] < menor)
+						menor = entrada[i];
+				}
+			}
+
+			return menor;
 		}
 	}
 }
